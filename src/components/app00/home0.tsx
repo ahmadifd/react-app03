78387b7509231be2f84d044047c613705d1180c9
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-10-31 14:43:23 +0330

    11

--- src/components/app00/home0.tsx
+++ src/components/app00/home0.tsx
@@ -1,10 +1,36 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import Button from "react-bootstrap/Button";
 
+const Child1 = () => {
+  useEffect(() => {}, []);
 
+  return (
+    <>
+      <div className="p-2">
+        <div className="m-5">
+          <Button as="a" variant="primary">
+            Button as link
+          </Button>
+          <Button as="a" variant="success">
+            Button as link
+          </Button>
+        </div>
+        <div className="m-5">
+          <button className="btn btn-primary"> Button as link</button>
+          <button className="btn btn-success"> Button as link</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 const Home0 = () => {
-    return ( <>
-    Home0
-    </> );
-}
- 
+  return (
+    <>
+      <Child1 />
+    </>
+  );
+};
+
 export default Home0;
